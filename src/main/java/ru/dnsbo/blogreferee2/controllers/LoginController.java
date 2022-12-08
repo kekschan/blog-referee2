5b30d6c913726c4a7aa444a270287c66390074fc
Denis Borshev <dnsborshev@gmail.com>
2022-12-08 11:40:33 +0300

    отредактировали внешний вид страницы профиля

--- src/main/java/ru/dnsbo/blogreferee2/controllers/LoginController.java
+++ src/main/java/ru/dnsbo/blogreferee2/controllers/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +19,7 @@ public class LoginController {
             return "redirect:/profile";
         }
         if (request.getParameterMap().containsKey("error")) {
-            model.addAttribute("error", true);
+            model.addAttribute("error", "Пользователь не найден");
         }/* else {
             model.addAttribute("error", false);
         }*/
--- src/main/java/ru/dnsbo/blogreferee2/controllers/RegistrationController.java
+++ src/main/java/ru/dnsbo/blogreferee2/controllers/RegistrationController.java
@@ -3,8 +3,10 @@ package ru.dnsbo.blogreferee2.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.dnsbo.blogreferee2.data.UsersRepository;
@@ -30,15 +32,12 @@ public class RegistrationController {
     }
 
     @PostMapping
-    private String PostRegistrationPage(RegistrationForm form) {
+    private String PostRegistrationPage(Model model, RegistrationForm form) {
 
         //проверка на существующий email
-        if(usersRepository.findByEmail(form.getEmail()).isPresent()){
+        if(usersRepository.findByEmail(form.getEmail()).isPresent()) {
             return "registration";
         }
-
-
-
         registrationService.registration(form);
         return "redirect:/login";
     }
